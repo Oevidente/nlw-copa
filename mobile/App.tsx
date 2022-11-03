7b9213bd85f21a41636166d4effc245474a07010
André Luiz Alves <andreluiz1902@gmail.com>
2022-11-03 14:19:57 -0300

    Adiciona Assets

---
+++ mobile/App.tsx
@@ -0,0 +1,24 @@
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, View } from 'react-native';
+
+export default function App() {
+  return (
+    <View style={styles.container}>
+      <Text style={styles.title}>Hello React Native!</Text>
+      <StatusBar style="auto" />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#D9D9D9',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  title: {
+    color: '#1A1A1A',
+    fontSize: 24,
+  },
+});
